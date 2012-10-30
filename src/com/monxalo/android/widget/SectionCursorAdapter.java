470fe9d8114edfd7ad3f0ab4c7274b63dd85966b
Gonçalo Ferreira <monxalo@gmail.com>
2012-10-30 17:37:00 +0000

    Support for latest Android version and minor fix.
    * Now inheriting CursorAdapter from the Support Library.
    * Added getItemId(position).

--- src/com/monxalo/android/widget/SectionCursorAdapter.java
+++ src/com/monxalo/android/widget/SectionCursorAdapter.java
@@ -1,5 +1,5 @@
 /***
-	Copyright 2011 Gonçalo Ferreira
+	Copyright 2011 GonÃ§alo Ferreira
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
@@ -12,7 +12,7 @@
    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
    See the License for the specific language governing permissions and
    limitations under the License.
-*/
+ */
 
 package com.monxalo.android.widget;
 
@@ -21,67 +21,68 @@ import java.util.LinkedHashMap;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.DataSetObserver;
+import android.support.v4.widget.CursorAdapter;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.CursorAdapter;
 import android.widget.TextView;
 
 public abstract class SectionCursorAdapter extends CursorAdapter {
 
 	private static final String TAG = "SectionCursorAdapter";
 	private static final boolean LOGV = false;
-	
+
 	private static final int TYPE_NORMAL = 1;
-    private static final int TYPE_HEADER = 0;
-    private static final int TYPE_COUNT = 2;
-	
-    private final int mHeaderRes;
-    private final int mGroupColumn;
-    private final LayoutInflater mLayoutInflater;
-    
-    private LinkedHashMap<Integer, String> sectionsIndexer;
-    
-    public static class ViewHolder {
-        public TextView textView;
-    }
-    
-	public SectionCursorAdapter(Context context, Cursor c, int headerLayout, int groupColumn) {
-		super(context, c);
-		
+	private static final int TYPE_HEADER = 0;
+	private static final int TYPE_COUNT = 2;
+
+	private final int mHeaderRes;
+	private final int mGroupColumn;
+	private final LayoutInflater mLayoutInflater;
+
+	private LinkedHashMap<Integer, String> sectionsIndexer;
+
+	public static class ViewHolder {
+		public TextView textView;
+	}
+
+	public SectionCursorAdapter(Context context, Cursor c, int headerLayout,
+			int groupColumn) {
+		super(context, c, 0);
+
 		sectionsIndexer = new LinkedHashMap<Integer, String>();
-		
+
 		mHeaderRes = headerLayout;
 		mGroupColumn = groupColumn;
 		mLayoutInflater = LayoutInflater.from(context);
-		
-		if(c != null) {
+
+		if (c != null) {
 			calculateSectionHeaders();
 			c.registerDataSetObserver(mDataSetObserver);
 		}
 	}
-	
+
 	private DataSetObserver mDataSetObserver = new DataSetObserver() {
 		public void onChanged() {
 			calculateSectionHeaders();
 		};
-		
+
 		public void onInvalidated() {
 			sectionsIndexer.clear();
 		};
-	}; 
-	
+	};
+
 	private void calculateSectionHeaders() {
 		int i = 0;
-		
+
 		String previous = "";
 		int count = 0;
-		
+
 		final Cursor c = getCursor();
-		
+
 		sectionsIndexer.clear();
-		
+
 		c.moveToPosition(-1);
 
 		while (c.moveToNext()) {
@@ -100,56 +101,56 @@ public abstract class SectionCursorAdapter extends CursorAdapter {
 			i++;
 		}
 	}
-	
+
 	public String getGroupCustomFormat(Object obj) {
 		return null;
 	}
-	
+
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
 		int viewType = getItemViewType(position);
-		
-		if( viewType == TYPE_NORMAL) {
+
+		if (viewType == TYPE_NORMAL) {
 			Cursor c = (Cursor) getItem(position);
-			
-			if(c == null)
+
+			if (c == null)
 				return mLayoutInflater.inflate(mHeaderRes, null);
-			
+
 			final int mapCursorPos = getSectionForPosition(position);
 			c.moveToPosition(mapCursorPos);
-			
+
 			return super.getView(mapCursorPos, convertView, parent);
-		} else {				 
-			
+		} else {
+
 			ViewHolder holder = null;
-			
+
 			if (convertView == null) {
 				if (LOGV)
 					Log.v(TAG, "Creating new view for section");
-				
-                holder = new ViewHolder();
-                convertView = mLayoutInflater.inflate(mHeaderRes, null);
-                holder.textView = (TextView) convertView;
-                
-                convertView.setTag(holder);
+
+				holder = new ViewHolder();
+				convertView = mLayoutInflater.inflate(mHeaderRes, null);
+				holder.textView = (TextView) convertView;
+
+				convertView.setTag(holder);
 			} else {
 				if (LOGV)
 					Log.v(TAG, "Reusing view for section");
-				
-				holder = (ViewHolder)convertView.getTag();
+
+				holder = (ViewHolder) convertView.getTag();
 			}
-			
-			TextView sectionText =  holder.textView;
-			
+
+			TextView sectionText = holder.textView;
+
 			final String group = sectionsIndexer.get(position);
-			final String customFormat = getGroupCustomFormat(group) ;
-			
+			final String customFormat = getGroupCustomFormat(group);
+
 			sectionText.setText(customFormat == null ? group : customFormat);
-			
+
 			return sectionText;
 		}
 	}
-	
+
 	@Override
 	public int getViewTypeCount() {
 		return TYPE_COUNT;
@@ -159,14 +160,14 @@ public abstract class SectionCursorAdapter extends CursorAdapter {
 	public int getCount() {
 		return super.getCount() + sectionsIndexer.size();
 	}
-	
+
 	@Override
 	public boolean isEnabled(int position) {
 		return getItemViewType(position) == TYPE_NORMAL;
 	}
 
 	public int getPositionForSection(int section) {
-		if(sectionsIndexer.containsKey(section)) {
+		if (sectionsIndexer.containsKey(section)) {
 			return section + 1;
 		}
 		return section;
@@ -174,25 +175,33 @@ public abstract class SectionCursorAdapter extends CursorAdapter {
 
 	public int getSectionForPosition(int position) {
 		int offset = 0;
-		for(Integer key: sectionsIndexer.keySet()) {
-			if(position > key) {
+		for (Integer key : sectionsIndexer.keySet()) {
+			if (position > key) {
 				offset++;
 			} else {
 				break;
 			}
 		}
-		
+
 		return position - offset;
 	}
-	
+
 	@Override
 	public Object getItem(int position) {
-		if (getItemViewType(position) == TYPE_NORMAL){
-            return super.getItem(getSectionForPosition(position));
-        } 
-        return super.getItem(position);
+		if (getItemViewType(position) == TYPE_NORMAL) {
+			return super.getItem(getSectionForPosition(position));
+		}
+		return super.getItem(position);
 	}
-	
+
+	@Override
+	public long getItemId(int position) {
+		if (getItemViewType(position) == TYPE_NORMAL) {
+			return super.getItemId(getSectionForPosition(position));
+		}
+		return super.getItemId(position);
+	}
+
 	@Override
 	public int getItemViewType(int position) {
 		if (position == getPositionForSection(position)) {
@@ -200,15 +209,29 @@ public abstract class SectionCursorAdapter extends CursorAdapter {
 		}
 		return TYPE_HEADER;
 	}
-	
+
 	@Override
 	public void changeCursor(Cursor cursor) {
-		if(getCursor() != null) {
+		final Cursor old = swapCursor(cursor);
+
+		if (old != null) {
+			old.close();
+		}
+	}
+
+	@Override
+	public Cursor swapCursor(Cursor newCursor) {
+		if (getCursor() != null) {
 			getCursor().unregisterDataSetObserver(mDataSetObserver);
 		}
-		
-		super.changeCursor(cursor);
-		calculateSectionHeaders();
-		cursor.registerDataSetObserver(mDataSetObserver);
+
+		final Cursor oldCursor = super.swapCursor(newCursor);
+
+		if (newCursor != null) {
+			calculateSectionHeaders();
+			newCursor.registerDataSetObserver(mDataSetObserver);
+		}
+
+		return oldCursor;
 	}
 }
